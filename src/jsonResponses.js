10ce97eb217a6a4bf9eace5282c8fc2e51a74bea
Jin Jin Heipler (RIT Student) <jxh9922@ad.rit.edu>
2019-09-20 15:09:35 -0400

    got rid of params in notFound

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,122 +1,117 @@
 const respond = (request, response, status, object, contentType) => {
-    console.log(contentType);
-    if (contentType[0] === 'text/xml') {
-        let responseXML = '<response>';
-        responseXML = `${responseXML} <message>${object.message}</message>`;
-        if (object.id) {
-            responseXML = `${responseXML} <id>${object.id}</id>`;
-        }
-        responseXML = `${responseXML} </response>`;
-
-        response.writeHead(status, {
-            'Content-Type': contentType,
-        });
-        response.write(responseXML);
-        response.end();
-    } else {
-        console.log(status, contentType);
-        response.writeHead(status, {
-            'Content-Type': contentType,
-        });
-        response.write(JSON.stringify(object));
-        response.end();
+  console.log(contentType);
+  if (contentType[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message>${object.message}</message>`;
+    if (object.id) {
+      responseXML = `${responseXML} <id>${object.id}</id>`;
     }
+    responseXML = `${responseXML} </response>`;
 
+    response.writeHead(status, {
+      'Content-Type': contentType,
+    });
+    response.write(responseXML);
+    response.end();
+  } else {
+    console.log(status, contentType);
+    response.writeHead(status, {
+      'Content-Type': contentType,
+    });
+    response.write(JSON.stringify(object));
+    response.end();
+  }
 };
 
 const success = (request, response, acceptedTypes) => {
-    const responseJSON = {
-        message: 'This is a successful response',
-    };
-    console.log("here");
-    respond(request, response, 200, responseJSON, acceptedTypes);
-    console.log(respond);
+  const responseJSON = {
+    message: 'This is a successful response',
+  };
+  console.log('here');
+  respond(request, response, 200, responseJSON, acceptedTypes);
+  console.log(respond);
 };
 
 const badRequest = (request, response, acceptedTypes, params) => {
-    const responseJSON = {
-        message: 'This request has the required parameters',
-    };
+  const responseJSON = {
+    message: 'This request has the required parameters',
+  };
 
-    if (!params.valid || params.valid !== 'true') {
-        responseJSON.message = 'Missing valid query parameter set to true';
-        responseJSON.id = 'badRequest';
+  if (!params.valid || params.valid !== 'true') {
+    responseJSON.message = 'Missing valid query parameter set to true';
+    responseJSON.id = 'badRequest';
 
-        respond(request, response, 400, responseJSON, acceptedTypes);
-        console.log(respond);
-    } else {
-        respond(request, response, 200, responseJSON, acceptedTypes);
-        console.log(respond);
-    }
-
-
+    respond(request, response, 400, responseJSON, acceptedTypes);
+    console.log(respond);
+  } else {
+    respond(request, response, 200, responseJSON, acceptedTypes);
+    console.log(respond);
+  }
 };
 
 const unauthorized = (request, response, acceptedTypes, params) => {
-    const responseJSON = {
-        message: 'This request has the required parameters',
-    };
+  const responseJSON = {
+    message: 'This request has the required parameters',
+  };
 
-    if (!params.valid || params.valid !== 'true') {
-        responseJSON.message = 'Missing valid query parameter set to true';
-        responseJSON.id = 'unauthorized';
+  if (!params.valid || params.valid !== 'true') {
+    responseJSON.message = 'Missing valid query parameter set to true';
+    responseJSON.id = 'unauthorized';
 
-        respond(request, response, 401, responseJSON, acceptedTypes);
-        console.log(respond);
-    } else {
-        respond(request, response, 200, responseJSON, acceptedTypes);
-        console.log(respond);
-    }
-
-
+    respond(request, response, 401, responseJSON, acceptedTypes);
+    console.log(respond);
+  } else {
+    respond(request, response, 200, responseJSON, acceptedTypes);
+    console.log(respond);
+  }
 };
 
 const forbidden = (request, response, acceptedTypes) => {
-    const responseJSON = {
-        id: 'forbidden',
-        message: 'You do not have access to this content.',
-    };
+  const responseJSON = {
+    id: 'forbidden',
+    message: 'You do not have access to this content.',
+  };
 
-    respond(request, response, 403, responseJSON, acceptedTypes);
-    console.log(respond);
+  respond(request, response, 403, responseJSON, acceptedTypes);
+  console.log(respond);
 };
 
 const internal = (request, response, acceptedTypes) => {
-    const responseJSON = {
-        id: 'internalError',
-        message: 'Internal Server Error. Something went wrong.',
-    };
+  const responseJSON = {
+    id: 'internalError',
+    message: 'Internal Server Error. Something went wrong.',
+  };
 
-    respond(request, response, 500, responseJSON, acceptedTypes);
-    console.log(respond);
+  respond(request, response, 500, responseJSON, acceptedTypes);
+  console.log(respond);
 };
 
 const notImplemented = (request, response, acceptedTypes) => {
-    const responseJSON = {
-        id: 'notImplemented',
-        message: 'A get request for this page has not been implemented yet. Check again later for updated content',
-    };
+  const responseJSON = {
+    id: 'notImplemented',
+    message: 'A get request for this page has not been implemented yet. Check again later for updated content',
+  };
 
-    respond(request, response, 501, responseJSON, acceptedTypes);
-    console.log(respond);
+  respond(request, response, 501, responseJSON, acceptedTypes);
+  console.log(respond);
 };
 
-const notFound = (request, response, acceptedTypes, params) => {
-    const responseJSON = {
-        id: 'notFound',
-        message: 'The page you are looking for was not found.',
-    };
+const notFound = (request, response, acceptedTypes) => {
+  const responseJSON = {
+    id: 'notFound',
+    message: 'The page you are looking for was not found.',
+  };
 
-    respond(request, response, 404, responseJSON, acceptedTypes);
-    console.log(respond);
+  respond(request, response, 404, responseJSON, acceptedTypes);
+  console.log(respond);
 };
 
 module.exports = {
-    success,
-    badRequest,
-    unauthorized,
-    forbidden,
-    internal,
-    notImplemented,
-    notFound,
-}
+  success,
+  badRequest,
+  unauthorized,
+  forbidden,
+  internal,
+  notImplemented,
+  notFound,
+};
--- src/server.js
+++ src/server.js
@@ -7,27 +7,27 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-    '/': htmlHandler.getIndex,
-    '/style.css': htmlHandler.getCSS,
-    '/success': jsonHandler.success,
-    '/badRequest': jsonHandler.badRequest,
-    '/unauthorized': jsonHandler.unauthorized,
-    '/forbidden': jsonHandler.forbidden,
-    '/internal': jsonHandler.internal,
-    '/notImplemented': jsonHandler.notImplemented,
-    notFound: jsonHandler.notFound,
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/success': jsonHandler.success,
+  '/badRequest': jsonHandler.badRequest,
+  '/unauthorized': jsonHandler.unauthorized,
+  '/forbidden': jsonHandler.forbidden,
+  '/internal': jsonHandler.internal,
+  '/notImplemented': jsonHandler.notImplemented,
+  notFound: jsonHandler.notFound,
 };
 
 const onRequest = (request, response) => {
-    const parsedUrl = url.parse(request.url);
-    const acceptedTypes = request.headers.accept.split(',');
-    const params = query.parse(parsedUrl.query);
+  const parsedUrl = url.parse(request.url);
+  const acceptedTypes = request.headers.accept.split(',');
+  const params = query.parse(parsedUrl.query);
 
-    if (urlStruct[parsedUrl.pathname]) {
-        urlStruct[parsedUrl.pathname](request, response, acceptedTypes, params);
-    } else {
-        urlStruct.notFound(request, response, acceptedTypes, params);
-    }
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, acceptedTypes, params);
+  } else {
+    urlStruct.notFound(request, response, acceptedTypes, params);
+  }
 };
 
 http.createServer(onRequest).listen(port);
